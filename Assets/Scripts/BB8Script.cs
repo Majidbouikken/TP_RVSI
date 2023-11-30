0c429c2a942b3949581b08d334f86a1836ca6e6d
Majidbouikken <bouikkenmajid@gmail.com>
2023-11-30 20:24:44 +0200

    Fix BB8 and add terrain

--- Assets/Scripts/BB8Script.cs
+++ Assets/Scripts/BB8Script.cs
@@ -1,23 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BB8Script : MonoBehaviour
 {
     public Transform player; // Reference de PlayerArmature
     public float followRadius = 3f; // Le rayon de suivi de BB8, par default a 3 metres
-    public float speed = 0.8f;
+    public float speed = 0.8f; // Le BB8 est plus lent que le joueur
+    public float headRotationSpeed = 5f;
     public float brakes = 3f;
 
+    private bool isActive = true; // on active ou desactive le BB8 selon ce boolean
     private Rigidbody rb;
     private Transform headTransform;
     private Transform bodyTransform;
+    private AudioSource audioSource;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponentInChildren<Rigidbody>();
         headTransform = transform.Find("BB8_tete");
         bodyTransform = transform.Find("BB8_corps");
+        audioSource = GetComponent<AudioSource>();
 
         if (rb == null)
         {
@@ -28,25 +30,37 @@ public class BB8Script : MonoBehaviour
 
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        float distanceToPlayer = Vector3.Distance(bodyTransform.position, player.position);
+        Vector3 directionToPlayer = (player.position - bodyTransform.position);
 
-        if (distanceToPlayer > followRadius)
+        if (Input.GetKeyDown(KeyCode.H))
         {
-            MoveBB8();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            isActive = !isActive;
+        }
+        if ((distanceToPlayer > followRadius) && isActive)
+        {
+            MoveBB8(directionToPlayer);
         }
         else if (distanceToPlayer <= followRadius && IsMoving())
         {
             StopBB8();
         }
+        MoveHead(directionToPlayer);
     }
 
-    void MoveBB8()
+    void MoveBB8(Vector3 directionToPlayer)
     {
-        Vector3 direction = (player.position - transform.position);
-        direction.y = 0;
-        direction.Normalize();
+        directionToPlayer.y = 0;
+        directionToPlayer.Normalize();
 
-        rb.AddForce(direction * speed, ForceMode.Acceleration);
+        rb.AddForce(directionToPlayer * speed, ForceMode.Acceleration);
     }
 
     void StopBB8()
@@ -59,6 +73,17 @@ public class BB8Script : MonoBehaviour
         {
             rb.velocity = Vector3.zero;
         }
+    }
+
+    void MoveHead(Vector3 directionToPlayer)
+    {
+        Vector3 newHeadPosition = new Vector3(bodyTransform.localPosition.x, bodyTransform.localPosition.y + 0.27f, bodyTransform.localPosition.z);
+        headTransform.position = newHeadPosition;
+
+        // Rotate head
+        float angle = Mathf.Atan2(directionToPlayer.x, directionToPlayer.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0f, angle, 0f);
+        headTransform.rotation = Quaternion.Slerp(headTransform.rotation, targetRotation, headRotationSpeed * Time.deltaTime);
     }
 
     bool IsMoving()
